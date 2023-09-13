e7f7d9972501d986ee994872d77ecccd6ec88cfc
VladyslavSiverskiy <v.siverskiy@gmail.com>
2023-09-13 09:59:28 +0300

    Add a couple of tests to HabitStatisticControllerTest.java, that check situations when the id habit is not found

--- core/src/test/java/greencity/controller/HabitStatisticControllerTest.java
+++ core/src/test/java/greencity/controller/HabitStatisticControllerTest.java
@@ -7,6 +7,8 @@ import greencity.converters.UserArgumentResolver;
 import greencity.dto.habitstatistic.AddHabitStatisticDto;
 import greencity.dto.habitstatistic.UpdateHabitStatisticDto;
 import greencity.dto.user.UserVO;
+import greencity.exception.exceptions.NotFoundException;
+import greencity.exception.handler.CustomExceptionHandler;
 import greencity.service.HabitStatisticService;
 import greencity.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +20,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
@@ -39,29 +43,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration
 @Import(SecurityConfig.class)
 class HabitStatisticControllerTest {
-
     private static final String habitStatisticControllerLink = "/habit/statistic";
-
     private MockMvc mockMvc;
-
     @InjectMocks
     private HabitStatisticController habitStatisticController;
-
     @Mock
     private HabitStatisticService habitStatisticService;
-
     @Mock
     private UserService userService;
-
     @Mock
     private ModelMapper modelMapper;
-
+    @Mock
+    private ObjectMapper objectMapper;
     private final Principal principal = getPrincipal();
+    private final ErrorAttributes errorAttributes = new DefaultErrorAttributes();
 
     @BeforeEach
     void setup() {
         this.mockMvc = MockMvcBuilders.standaloneSetup(habitStatisticController)
                 .setCustomArgumentResolvers(new UserArgumentResolver(userService, modelMapper))
+                .setControllerAdvice(new CustomExceptionHandler(errorAttributes, objectMapper))
                 .build();
     }
 
@@ -71,29 +72,57 @@ class HabitStatisticControllerTest {
         when(userService.findByEmail(anyString())).thenReturn(userVO);
         when(modelMapper.map(userVO, UserVO.class)).thenReturn(userVO);
 
-        String json = "{\n" +
-                      "  \"amountOfItems\": 1,\n" +
-                      "  \"createDate\": \"2023-09-12T17:14:28.616Z\",\n" +
-                      "  \"habitRate\": \"DEFAULT\"\n" +
-                      "}";
+        String content = "{\n" +
+                         "  \"amountOfItems\": 1,\n" +
+                         "  \"createDate\": \"2023-09-12T17:14:28.616Z\",\n" +
+                         "  \"habitRate\": \"DEFAULT\"\n" +
+                         "}";
 
-        mockMvc.perform(post(habitStatisticControllerLink + "/{habitId}",1)
+        mockMvc.perform(post(habitStatisticControllerLink + "/{habitId}", 1)
                         .principal(principal)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
+                        .content(content))
                 .andExpect(status().isCreated());
 
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
-        AddHabitStatisticDto addHabitStatisticDto = mapper.readValue(json, AddHabitStatisticDto.class);
+        AddHabitStatisticDto addHabitStatisticDto = mapper.readValue(content, AddHabitStatisticDto.class);
 
         verify(habitStatisticService)
                 .saveByHabitIdAndUserId(1L, userVO.getId(), addHabitStatisticDto);
     }
 
     @Test
+    void saveHabitStatisticHabitNotFoundTest() throws Exception {
+        UserVO userVO = getUserVO();
+        when(userService.findByEmail(anyString())).thenReturn(userVO);
+        when(modelMapper.map(userVO, UserVO.class)).thenReturn(userVO);
+
+        String content = "{\n" +
+                         "  \"amountOfItems\": 2,\n" +
+                         "  \"createDate\": \"2023-10-11T17:14:28.616Z\",\n" +
+                         "  \"habitRate\": \"DEFAULT\"\n" +
+                         "}";
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        AddHabitStatisticDto addHabitStatisticDto = mapper.readValue(content, AddHabitStatisticDto.class);
+
+        when(habitStatisticService.saveByHabitIdAndUserId(2L, userVO.getId(), addHabitStatisticDto))
+                .thenThrow(NotFoundException.class);
+
+        mockMvc.perform(post(habitStatisticControllerLink + "/{habitId}", 2)
+                        .principal(principal)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isNotFound());
+
+        verify(habitStatisticService)
+                .saveByHabitIdAndUserId(2L, userVO.getId(), addHabitStatisticDto);
+    }
+
+    @Test
     void saveHabitStatisticBadRequestTest() throws Exception {
-        mockMvc.perform(post(habitStatisticControllerLink + "/{habitId}",1)
+        mockMvc.perform(post(habitStatisticControllerLink + "/{habitId}", 1)
                         .principal(principal)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{}"))
@@ -124,6 +153,35 @@ class HabitStatisticControllerTest {
         verify(habitStatisticService)
                 .update(1L, userVO.getId(), updateHabitStatisticDto);
     }
+
+    @Test
+    void updateHabitStatisticHabitNotFoundTest() throws Exception {
+        UserVO userVO = getUserVO();
+        when(userService.findByEmail(anyString())).thenReturn(userVO);
+        when(modelMapper.map(userVO, UserVO.class)).thenReturn(userVO);
+
+        String content = "{\n" +
+                         "  \"amountOfItems\": 0,\n" +
+                         "  \"habitRate\": \"DEFAULT\"\n" +
+                         "}";
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        UpdateHabitStatisticDto updateHabitStatisticDto = mapper.readValue(content, UpdateHabitStatisticDto.class);
+
+        when(habitStatisticService.update(2L, userVO.getId(), updateHabitStatisticDto))
+                .thenThrow(NotFoundException.class);
+
+        mockMvc.perform(put(habitStatisticControllerLink + "/{id}", 2)
+                        .principal(principal)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isNotFound());
+
+        verify(habitStatisticService)
+                .update(2L, userVO.getId(), updateHabitStatisticDto);
+    }
+
 
     @Test
     void updateStatisticsBadRequestTest() throws Exception {
