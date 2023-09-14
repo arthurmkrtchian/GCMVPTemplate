93a5cf352d5ebad173f724ef7c742be36bf35b4c
Zhenyettta <zhecky2004@gmail.com>
2023-09-14 14:33:04 +0300

    Expanded EcoNewsDtoRequestValidatorTest with additional cases

--- core/src/test/java/greencity/validator/EcoNewsDtoRequestValidatorTest.java
+++ core/src/test/java/greencity/validator/EcoNewsDtoRequestValidatorTest.java
@@ -1,12 +1,17 @@
 package greencity.validator;
 
 import greencity.dto.econews.AddEcoNewsDtoRequest;
+import greencity.exception.exceptions.InvalidURLException;
+import greencity.exception.exceptions.WrongCountOfTagsException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Collections;
+
 import static greencity.ModelUtils.getAddEcoNewsDtoRequest;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
@@ -19,5 +24,26 @@ class EcoNewsDtoRequestValidatorTest {
         AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
         request.setSource("https://eco-lavca.ua/");
         assertTrue(validator.isValid(request, null));
+    }
+
+    @Test
+    public void testInvalidSourceUrl() {
+        AddEcoNewsDtoRequest requestWithInvalidUrl = getAddEcoNewsDtoRequest();
+        requestWithInvalidUrl.setSource("invalidurl");
+        assertThrows(InvalidURLException.class, () -> validator.isValid(requestWithInvalidUrl, null));
+    }
+
+    @Test()
+    public void testEmptyTags() {
+        AddEcoNewsDtoRequest requestWithNullTags = getAddEcoNewsDtoRequest();
+        requestWithNullTags.setTags(Collections.emptyList());
+        assertThrows(WrongCountOfTagsException.class, () -> validator.isValid(requestWithNullTags, null));
+    }
+
+    @Test
+    public void testTooManyTags() {
+        AddEcoNewsDtoRequest requestWithNullTags = getAddEcoNewsDtoRequest();
+        requestWithNullTags.setTags(Collections.nCopies(4, "Tag"));
+        assertThrows(WrongCountOfTagsException.class, () -> validator.isValid(requestWithNullTags, null));
     }
 }
